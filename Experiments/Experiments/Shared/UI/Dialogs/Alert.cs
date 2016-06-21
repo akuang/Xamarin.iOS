6d5821e2a8016be4390c3fefd098ffb418a8cb61
Alex Kuang <eexteam@EEXTeam.local>
2016-06-21 10:30:41 -0700

    Adding Shared folder with shared Alert class

---
+++ Experiments/Experiments/Shared/UI/Dialogs/Alert.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UIKit;
+
+namespace Xamarin.iOS.Experiments
+{
+    public class Alert
+    {
+		/// <summary>
+		/// Shows Alert dialog with specified titile and message.
+		/// </summary>
+		/// <remarks><see cref="UIAlertView"/> API is deprecated in iOS 9.0</remarks>
+		/// <param name="titile">Titile.</param>
+		/// <param name="message">Message.</param>
+		public static UIAlertView Message(string titile, string message)
+        {
+            UIAlertView alertView = new UIAlertView(
+                titile,
+                message,
+                null,
+                "OK",
+                null);
+            alertView.Show();
+
+            return alertView;
+        }
+
+        /// <summary>
+        /// Shows Error Alert dialog with specified message.
+        /// </summary>
+        /// <param name="message">Message.</param>
+        public static UIAlertView Error(string message)
+        {
+            return Message("Error!", message);
+        }
+
+		/// <summary>
+		/// Shows Success Alert dialog with specified message.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		public static UIAlertView Success(string message)
+		{
+			return Message("Success!", message);
+		}
+
+	}
+}
+
